b9375d57f5dd57d0a244fff83358490e787e47d3
Abdullah Furkan Titiz <65949007+afurkantitiz@users.noreply.github.com>
2021-10-29 21:17:30 +0300

    Added bottom navigation bar

--- app/src/main/java/com/example/casestudy/ui/MainActivity.kt
+++ app/src/main/java/com/example/casestudy/ui/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.casestudy.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.casestudy.R
+import com.example.casestudy.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        navController = Navigation.findNavController(this, R.id.navHostFragment)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
